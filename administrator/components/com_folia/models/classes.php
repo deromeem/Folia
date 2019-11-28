71a04aa70a5783df2989af3050c93afe9b13f6f7
Jordan Etinault <jordan.etinault@squill.fr>
2019-11-28 13:10:07 +0100

    Add MVC Classes

---
+++ administrator/components/com_folia/models/classes.php
@@ -0,0 +1,65 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+class FoliaModelClasses extends JModelList
+{
+	public function __construct($config = array())
+	{
+		// précise les colonnes activant le tri
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'c.id',
+				'libelle', 'c.libelle',
+				'published', 'c.published',
+				'hits', 'c.hits',
+				'modified', 'c.modified'
+			);
+		}
+		parent::__construct($config);
+	}
+
+	protected function populateState($ordering = null, $direction = null)
+	{
+		// récupère les informations de la session utilisateur nécessaires au paramétrage de l'écran
+		$search = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
+
+		// parent::populateState('modified', 'desc');
+		parent::populateState('c.libelle', 'ASC');
+	}
+	
+	protected function getListQuery()
+	{
+		// construit la requête d'affichage de la liste
+		$query = $this->_db->getQuery(true);
+		$query->select('c.id, c.libelle, c.published, c.hits, c.modified');
+		$query->from('#__folia_classes c');
+
+		// filtre de recherche rapide textuelle
+		$search = $this->getState('filter.search');
+		if (!empty($search)) {
+			// recherche prefixée par 'id:'
+			if (stripos($search, 'id:') === 0) {
+				$query->where('c.id = '.(int) substr($search, 3));
+			}
+			else {
+				// recherche textuelle classique (sans préfixe)
+				$search = $this->_db->Quote('%'.$this->_db->escape($search, true).'%');
+				// Compile les clauses de recherche
+				$searches	= array();
+				$searches[]	= 'c.libelle LIKE '.$search;
+				// Ajoute les clauses à la requête
+				$query->where('('.implode(' OR ', $searches).')');
+			}
+		}
+
+		// tri des colonnes
+		$orderCol = $this->state->get('list.ordering', 'c.libelle');
+		$orderDirn = $this->state->get('list.direction', 'ASC');
+		$query->order($this->_db->escape($orderCol.' '.$orderDirn));
+
+		// echo nl2br(str_replace('#__','egs_',$query));			// TEST/DEBUG
+		return $query;
+	}
+}
