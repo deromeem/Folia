8676cf64c03b110c5cb825d1b84673f7ed63a56b
Jordan Etinault <jordan.etinault@squill.fr>
2019-11-28 13:09:33 +0100

    Add MVC professeurs

---
+++ administrator/components/com_folia/models/professeurs.php
@@ -0,0 +1,72 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+class FoliaModelprofesseurs extends JModelList
+{
+	public function __construct($config = array())
+	{
+		// précise les colonnes activant le tri
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'p.id',
+				'email', 'p.email',
+				'published', 'p.published',
+				'hits', 'p.hits',
+				'modified', 'p.modified'
+			);
+		}
+		parent::__construct($config);
+	}
+
+	protected function populateState($ordering = null, $direction = null)
+	{
+		// récupère les informations de la session utilisateur nécessaires au paramétrage de l'écran
+		$search = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
+
+		// parent::populateState('modified', 'desc');
+		parent::populateState('p.email', 'ASC');
+		parent::populateState('u.nom', 'ASC');
+		parent::populateState('u.prenom', 'ASC');
+	}
+	
+	protected function getListQuery()
+	{
+		// construit la requête d'affichage de la liste
+		$query = $this->_db->getQuery(true);
+		$query->select('p.id, p.email, p.published, p.hits, p.modified');
+		$query->from('#__folia_professeurs p');
+
+		$query->select('u.nom, u.prenom')->join('LEFT', '#__folia_utilisateurs AS u ON u.email=p.email');
+
+		// filtre de recherche rapide textuelle
+		$search = $this->getState('filter.search');
+		if (!empty($search)) {
+			// recherche prefixée par 'id:'
+			if (stripos($search, 'id:') === 0) {
+				$query->where('p.id = '.(int) substr($search, 3));
+			}
+			else {
+				// recherche textuelle classique (sans préfixe)
+				$search = $this->_db->Quote('%'.$this->_db->escape($search, true).'%');
+				// Compile les clauses de recherche
+				$searches	= array();
+				$searches[]	= 'p.email LIKE '.$search;
+				$searches[]	= 'u.nom LIKE '.$search;
+				$searches[]	= 'u.prenom LIKE '.$search;
+				// Ajoute les clauses à la requête
+				$query->where('('.implode(' OR ', $searches).')');
+			}
+		}
+
+		// tri des colonnes
+		$orderCol = $this->state->get('list.ordering', 'p.email');
+		$orderCol = $this->state->get('list.ordering', 'u.nom');
+		$orderDirn = $this->state->get('list.direction', 'ASC');
+		$query->order($this->_db->escape($orderCol.' '.$orderDirn));
+
+		// echo nl2br(str_replace('#__','egs_',$query));			// TEST/DEBUG
+		return $query;
+	}
+}
