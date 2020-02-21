a15c47b90b5cfe3d5830e40fda8df68ecb27fbd0
Emmanuel DEROME <emmanuel.derome@gmail.com>
2020-02-21 22:43:47 +0100

    ajout du dossier api

---
+++ api/application/folia.php
@@ -0,0 +1,159 @@
+<?php
+ 
+defined('_JEXEC') or die;
+
+JLoader::import('joomla.user.authentication');
+JLoader::import('joomla.application.component.helper');
+
+class AppFoliaWeb extends JApplicationCms
+{
+	public function __construct()
+	{
+		parent::__construct();
+		require_once JPATH_CONFIGURATION.'/configuration.php';
+	}
+
+	private function LoadViewResult($task, $id = 0, $did = 0, $email = "")
+	{
+		$this->_db = JFactory::getDBO();
+		$query = $this->_db->getQuery(true);
+		$query->select('*');
+		$query->from($this->_db->quoteName('#__vue_'.$task));
+		if ($id != 0){
+			$query->where('id = '.$id);
+		}
+		if ($did != 0){
+			$query->where('did = '.$did);
+		}
+		if ($email != ""){
+			$query->where('email = "'.$email.'"');
+		}
+		// echo nl2br(str_replace('#__','folia_',$query));			// TEST/DEBUG
+		try {
+			$this->_db->setQuery($query);
+			return $this->_db->loadObjectList();
+		}
+		catch (exception $e) {
+			return 0;
+		}
+	}
+	
+	public function doExecute()
+	{
+		// Initialisation du tableau pour la réponse JSON :
+		$response = array();
+		
+		// Initialisation des paramètres de l'url :
+		$login = "";
+		$pwd = "";
+		
+		$task = "";
+		$id = 0;
+		$did = 0;
+		
+		// Récupération des paramètres de connexion de l'url (en GET ou POST) :
+		if ((isset($_GET["login"])) and (isset($_GET["pwd"]))) {
+			$login = $_GET['login'];
+			$pwd = $_GET['pwd'];
+		} elseif ((isset($_POST["login"])) and (isset($_POST["pwd"]))) {
+			$login = $_POST['login'];
+			$pwd = $_POST['pwd'];
+		}
+		// Récupération du paramètre d'action, si existant, de l'url (en GET ou POST) :
+		if (isset($_GET["task"])) {
+			$task = $_GET['task'];
+		} elseif (isset($_POST["task"])) {
+			$task = $_POST['task'];
+		}
+		// Récupération du paramètre id, si existant, de l'url (en GET ou POST) :
+		if (isset($_GET["id"])) {
+			$id = $_GET['id'];
+		} elseif (isset($_POST["id"])) {
+			$id = $_POST['id'];
+		}
+		// Récupération du paramètre did, si existant, de l'url (en GET ou POST) :
+		if (isset($_GET["did"])) {
+			$did = $_GET['did'];
+		} elseif (isset($_POST["did"])) {
+			$did = $_POST['did'];
+		}
+		
+		// echo ("DEBUG login = " . $login . " pwd = " . $pwd . " task =>" . $task . " id =>" . $id . "<");    // TEST/DEBUG
+	
+		if (($login != "") and ($pwd != "")) {
+
+			$credentials = array();
+			$credentials['username']  = $login;
+			$credentials['password']  = $pwd;
+			$credentials['secretkey'] = '';      // 6 digits user secret for Two-Factor Authentication
+
+			$config = JFactory::getConfig();
+			$authenticate = JAuthentication::getInstance();
+			$resp = $authenticate->authenticate($credentials);
+			
+			if ($resp->status !== 1){
+				// Echec de connexion !
+				$response["success"] = 0;
+				$response["message"] = "Echec de connexion";
+				$response["user"] = "";		
+			}else{
+				// Connexion réussie !
+				$response["success"] = 1;
+				$response["message"] = "Connexion reussie";
+				
+				// Recherche le nom, l'email et le groupe de l'utilisateur connecté :
+				$this->_db = JFactory::getDBO();
+				$query = $this->_db->getQuery(true);
+				
+				$query->select('u.id AS uid, u.name AS name, u.email AS email');
+				$query->from('#__users u');
+				$query->select('ugm.group_id AS ugid')->join('LEFT', '#__user_usergroup_map AS ugm ON ugm.user_id=u.id');				
+				$query->select('ug.title AS frgroup')->join('LEFT', '#__usergroups AS ug ON ug.id=ugm.group_id');				
+				$query->where('u.username = "'.$login.'"');
+				$query->where('ugm.group_id > 10');
+				
+				$this->_db->setQuery($query);
+				$response["user"] = $this->_db->loadObject();
+				
+				// Supprime le premier mot ("Folia") du nom du groupe :
+				$gwords = explode(" ", $response["user"]->frgroup);
+				$response["user"]->frgroup = $gwords[1];
+				
+				// Recherche la vue éventuellement demandée :
+				if ($task !== ""){
+					$task_prefix = "";
+					$twords = explode("_", $task);
+					if (count($twords) > 1){						
+						$task_prefix = $twords[0];
+					}
+					// echo ("DEBUG task_prefix =>" . $task_prefix . "< ");   // TEST/DEBUG
+					if (($task_prefix == "mes") or ($task_prefix == "mon") or ($task_prefix == "ma")) {
+						$email = $response["user"]->email;
+					} else {
+						$email = "";				
+					}
+					$response[$task] = $this->LoadViewResult($task, $id, $did, $email);
+					
+					if ($response[$task] == 0){
+						// Vue inexistante :
+						$response["success"] = 0;
+						$response["message"] = "Vue_" . $task . " inexistante";
+					}
+				}
+			}
+		} else {
+			// Identifiant(s) de connexion manquant(s) :
+			$response["success"] = 0;
+			$response["message"] = "Identifiant(s) de connexion manquant(s)";
+			$response["user"] = "";
+		}
+		echo json_encode($response);
+	}
+	
+	public function isAdmin()
+	{
+		// fonction appelée quand l'utilisateur est connecté
+		return false;
+    }
+
+}
---
+++ api/index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2016 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+define('JOOMLA_MINIMUM_PHP', '5.3.10');
+
+if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
+{
+	die('Your host needs to use PHP ' . JOOMLA_MINIMUM_PHP . ' or higher to run this version of Joomla!');
+}
+
+// Enregistre l'heure courante et la quantité de mémoire utilisée par PHP :
+$startTime = microtime(1);
+$startMem  = memory_get_usage();
+
+define('_JEXEC', 1);
+
+if (file_exists(__DIR__ . '/defines.php'))
+{
+	include_once __DIR__ . '/defines.php';
+}
+
+if (!defined('_JDEFINES'))
+{
+	define('JPATH_BASE', __DIR__);
+	require_once JPATH_BASE . '/includes/defines.php';
+}
+// echo nl2br("JPATH_BASE : " . JPATH_BASE);			// TEST/DEBUG
+
+// Chargement du Framework Joomla :
+require_once JPATH_BASE . '/includes/framework.php';
+
+// Définit dans le profiler l'heure de début et la mémoire utilisée et la marque afterLoad :
+JDEBUG ? JProfiler::getInstance('Application')->setStart($startTime, $startMem)->mark('afterLoad') : null;
+
+// Chargement de l'application :
+require_once JPATH_BASE . '/application/folia.php';
+$app = JApplicationWeb::getInstance('AppFoliaWeb');
+JFactory::$application = $app;
+$app->doExecute();
