8d6f159a16945d4d475441556a96640da12030d2
Ahmed-reda Mokhtari <ahmed-reda123@hotmail.fr>
2020-03-17 16:09:13 +0100

    model Protfolios

---
+++ administrator/components/com_folia/models/portfolios.php
@@ -0,0 +1,70 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+class FoliaModelPortfolios extends JModelList
+{
+	public function __construct($config = array())
+	{
+		// précise les colonnes activant le tri
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'p.id',
+				'libelle', 'p.libelle',
+				'etudiant_id', 'p.etudiant_id',
+				'themes_id', 'p.email',
+				'alias', 'p.alias',
+				'published', 'p.published',
+				'hits', 'p.hits',
+				'modified', 'p.modified'
+			);
+		}
+		parent::__construct($config);
+	}
+
+	protected function populateState($ordering = null, $direction = null)
+	{
+		// récupère les informations de la session utilisateur nécessaires au paramétrage de l'écran
+		$search = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
+
+		// parent::populateState('modified', 'desc');
+		parent::populateState('p.id', 'ASC');
+	}
+	
+	protected function getListQuery()
+	{
+		// construit la requête d'affichage de la liste
+		$query = $this->_db->getQuery(true);
+		$query->select('p.id, p.libelle, p.etudiant_id, u.themes_id, p.alias, p.published, p.hits, p.modified');
+		$query->from('#__folia_portfolios p');
+
+		// filtre de recherche rapide textuelle
+		$search = $this->getState('filter.search');
+		if (!empty($search)) {
+			// recherche prefixée par 'id:'
+			if (stripos($search, 'id:') === 0) {
+				$query->where('p.id = '.(int) substr($search, 3));
+			}
+			else {
+				// recherche textuelle classique (sans préfixe)
+				$search = $this->_db->Quote('%'.$this->_db->escape($search, true).'%');
+				// Compile les clauses de recherche
+				$searches	= array();
+				$searches[]	= 'p.libelle LIKE '.$search;
+				$searches[]	= 'p.etudiant_id LIKE '.$search;
+				$searches[]	= 'p.themes_id LIKE '.$search;
+				// Ajoute les clauses à la requête
+				$query->where('('.implode(' OR ', $searches).')');
+			}
+		}
+
+		// tri des colonnes
+		$orderCol = $this->state->get('list.ordering', 'p.libelle');
+		$orderDirn = $this->state->get('list.direction', 'ASC');
+		$query->order($this->_db->escape($orderCol.' '.$orderDirn));
+
+		// echo nl2br(str_replace('#__','egs_',$query));			// TEST/DEBUG
+		return $query;
+	}
+}
